36018caabc0a5c6dbfed0f5c5c7da63f6b8ca2b8
KimTyun <106860407+KimTyun@users.noreply.github.com>
2025-09-17 18:16:00 +0900

    [feact] 리워드 api 추가

---
+++ routes/reward.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const { RewardItem, User, Reward, sequelize, RewardRecord } = require('../models')
+const { isLoggedIn } = require('../middleware/middleware')
+const router = express.Router()
+
+//리워드 리스트 가져오기
+router.get('/', async (req, res, next) => {
+   try {
+      const rewards = await RewardItem.findAll()
+
+      res.json({
+         success: true,
+         data: rewards,
+      })
+   } catch (error) {
+      next(error)
+   }
+})
+
+//코인 교환하기
+router.put('/coin', isLoggedIn, async (req, res, next) => {
+   const transaction = await sequelize.transaction()
+   try {
+      const userPoint = await Reward.findOne({ where: { id: req.user.id }, transaction })
+      const count = req.body.count
+      if (userPoint.point < count * 1000) {
+         const error = new Error('포인트가 부족합니다.')
+         error.status = 400
+         throw error
+      }
+      await userPoint.update(
+         {
+            coin: userPoint.coin + count,
+            point: userPoint.point - count * 1000,
+         },
+         { transaction }
+      )
+      await RewardRecord.create(
+         {
+            change: -(count * 1000),
+            reason: 'SLC 교환',
+         },
+         { transaction }
+      )
+
+      transaction.commit()
+      res.json({
+         success: true,
+      })
+   } catch (error) {
+      transaction.rollback()
+      next(error)
+   }
+})
+
+//리워드 교환하기
+router.put('/:id', isLoggedIn, async (req, res, next) => {
+   const transaction = await sequelize.transaction()
+   try {
+      const userPoint = await Reward.findOne({ where: { id: req.user.id }, transaction })
+      const rewardId = req.body.rewardId
+      const reward = await RewardItem.findByPk(rewardId, { transaction })
+      if (!reward) {
+         const error = new Error('해당 리워드를 찾을 수 없습니다.')
+         error.status = 404
+         throw error
+      }
+
+      if (userPoint.point < reward.point) {
+         const error = new Error('포인트가 부족합니다.')
+         error.status = 400
+         throw error
+      }
+
+      await userPoint.update(
+         {
+            point: userPoint.point - reward.point,
+         },
+         { transaction }
+      )
+
+      await RewardRecord.create(
+         {
+            change: -reward.point,
+            reason: '상품 교환',
+            reward_item_id: rewardId,
+         },
+         { transaction }
+      )
+      transaction.commit()
+      res.json({
+         success: true,
+      })
+   } catch (error) {
+      transaction.rollback()
+      next(error)
+   }
+})
+
+module.exports = router
